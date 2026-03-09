969d5b06e1064ef0466145f9459a52c8b8353171
Mario Rosales <marrr.17@icloud.com>
2026-03-09 07:56:12 -0600

    Add heat validation and sample readiness checks

--- main.py
+++ main.py
@@ -4,8 +4,6 @@ with open("test_data.json", "r") as file:
     chemistry_data = json.load(file)
 workbook = openpyxl.load_workbook("heatlog_test.xlsx")
 sheet = workbook.active
-print(sheet["B1"].value)
-print(chemistry_data["2610410"]["P1"]["C"])
 column_map = {
                       "C": "B",
                       "Mn": "C",
@@ -24,15 +22,21 @@ row_map = {
                       "P4": 5,
                       "P5": 6
                  }
-for heat in chemistry_data:
-        for sample in chemistry_data[heat]:
-            for element in chemistry_data[heat][sample]:
-                 column = column_map[element]
-                 row = row_map[sample]
-                 cell = column + str(row)
-                 value = chemistry_data[heat][sample][element]
-                 sheet[cell] = value
-workbook.save("heatlog_test.xlsx")
+heat = input("Enter heat number")
+if heat not in chemistry_data:
+    print('Heat not found')
+elif "P1" not in chemistry_data[heat] or "P2" not in chemistry_data[heat] or "P3" not in chemistry_data[heat]:
+    print("Waiting on samples")
+
+else:
+    for sample in chemistry_data[heat]:
+        for element in chemistry_data[heat][sample]:
+            column = column_map[element]
+            row = row_map[sample]
+            cell = column + str(row)
+            value = chemistry_data[heat][sample][element]
+            sheet[cell] = value
+    workbook.save("heatlog_test.xlsx")
 # Mapping - Element = columns samples = rows
 #main quest get test_data into an excel spreadsheet 
 #side quest - use nested dictionaries to find the value of each samples chemistry
